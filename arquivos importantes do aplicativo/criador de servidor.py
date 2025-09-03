83311a9ab110a57fadab0befa91470ca1f5e8c40
aluapana135-design <aluapana135@gmail.com>
2025-09-02 21:19:38 -0300

    Atualizar o criador de servidor.py

--- arquivos importantes do aplicativo/criador de servidor.py
+++ arquivos importantes do aplicativo/criador de servidor.py
@@ -1,13 +1,7 @@
 from flask import Flask, jsonify
-
 app = Flask(__name__)
-
-
 @app.route('/dados')
-def get_data():
-    
+def get_data():  
     return jsonify(response)
-
-# O servidor roda no seu computador, acessível na rede local pela porta 5000.
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
